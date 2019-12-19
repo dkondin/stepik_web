ff5b9ac725d25f102f8870e9eeb771de1b3edf25
Diana Kondinskaia <kondinskaia@biocad.ru>
2019-12-19 17:12:17 +0300

    step 11

---
+++ hello.py
@@ -0,0 +1,8 @@
+def wsgi_application(environ, start_response):
+    status = '200 OK'
+    headers = [('Content-Type', 'text/plain')]
+
+    body = environ['QUERY_STRING'][:2].strip().split("&")
+    start_response(status, headers)
+    return ("\n").join(body)
+
