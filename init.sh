064b35750c2c2e51450550fcc73d999173316be9
Diana Kondinskaia <kondinskaia@biocad.ru>
2019-12-19 09:20:56 +0300

    step 12

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo rm -rf /etc/nginx/sites-enabled/default
+sudo unlink /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
