ef7304d5f269245ab2fb8d402985e3ba07c09b70
Diana Kondinskaia <kondinskaia@biocad.ru>
2019-12-19 17:03:49 +0300

    step 11

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo unlink /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-sudo ln -sf /home/box/web/etc/hello.oy   /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
