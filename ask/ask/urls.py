4d557ca9c3fa65b93f57d0172e58b23a881c5a74
Diana Kondinskaia <kondinskaia@biocad.ru>
2019-12-20 11:05:51 +0300

    step 11

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -21,7 +21,7 @@ urlpatterns = patterns(
     url(r"^$", "test"),
     url(r"^login/.*$", "test", name="login"),
     url(r"^signup/.*", "test", name="signup"),
-    url(r"^question/(?P<id>[0-9]+)/$", "test", name="question"),
+    url(r"^question/\d+/$", "test", name="question"),
     url(r"^ask/.*", "test", name="ask"),
     url(r"^popular/.*", "test", name="popular"),
     url(r"^new/.*", "test", name="new"),
