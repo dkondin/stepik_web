938b5e576c3db4f260df4b33069e0a0d8bcbc0fa
Diana Kondinskaia <kondinskaia@biocad.ru>
2019-12-24 08:43:20 +0300

    step 11

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -4,11 +4,11 @@ from django.contrib import admin
 from qa.views import test
 
 urlpatterns = [
-    url(r"^$", "test", name='index'),
-    url(r"^login/.*$", "test", name="login"),
-    url(r"^signup/.*", "test", name="signup"),
-    url(r"^question/\d+/$", "test", name="question"),
-    url(r"^ask/.*", "test", name="ask"),
-    url(r"^popular/.*", "test", name="popular"),
-    url(r"^new/.*", "test", name="new"),
+    url(r"^$", include("test")),
+    url(r"^login/.*$", include("test")),
+    url(r"^signup/.*", include("test")),
+    url(r"^question/\d+/$", include("test")),
+    url(r"^ask/.*", include("test")),
+    url(r"^popular/.*", include("test")),
+    url(r"^new/.*", include("test")),
 ]
