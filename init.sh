dd775b42c15d5343c6777a8e11e8abaa4f5ef1be
Diana Kondinskaia <kondinskaia@biocad.ru>
2019-12-24 12:58:49 +0300

    step 10

--- init.sh
+++ init.sh
@@ -4,3 +4,9 @@ sudo /etc/init.d/nginx restart
 
 sudo ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
+
+sudo /etc/init.d/mysql start
+mysql -uroot -e "create database stepik_web;"
+mysql -uroot -e "grant all privileges on stepik_web.* to 'box'@'localhost' with grant option;"
+~/web/ask/manage.py makemigrations
+~/web/ask/manage.py migrate
